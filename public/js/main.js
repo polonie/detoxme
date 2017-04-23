716c3d1957f28a55b76b2530a2720f7b451e1ef2
polonie <lander89@inbox.ru>
2017-04-23 17:47:12 +0600

    .

--- public/js/main.js
+++ public/js/main.js
@@ -20,9 +20,17 @@ $(document).ready(function(){
 		slidesToShow: 5,
 		slidesToScroll: 5
 	});
-	$('.more-button').click(function(e) {
-		$(this).parent().children(':first-child').css('display', 'block');
-	});
+	var mouse_in = function(e) {
+		$(this).parent().children('.plan-block-body-item-hover').fadeIn(200);
+	};
+	var mouse_out = function(e) {
+		$(this).fadeOut(200);
+	}
+	$('.more-button').mouseover(mouse_in);
+	$('.plan-block-body-item-hover').mouseout(mouse_out);
+
+
+
 	$('.nav-link').click(function(e){
 		e.preventDefault();
 		var linkHref = $(this).attr('href');
@@ -36,60 +44,49 @@ $(document).ready(function(){
 			.animate({opacity: "1"}, 300);
 			$('.popup-window').animate({top: "+=100px"}, 300);
 	});
-	function sendOrder(e){
-		var val1 = $.trim($('#order-form-input-name').val()).length;
-		var val2 = $.trim($('#order-form-input-phone').val()).length;
-		if (val1 > 0 && val2 > 0){
-			var submitbutton = $('#order-form-submit');
-			submitbutton.off('click', sendOrder).removeClass('enabled').addClass('disabled').text('Полождите...');
-			emailjs.send('test_email','test_email_template',{name: "James", phone: "Check that out!"})
-			.then(function(response) {
-					console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
-					submitbutton.removeClass('disabled').addClass('enabled').text('Отправить').on('click', sendOrder);
-					$('#return-message').text('Сообщение отправлено').css('color','#45e45b').fadeIn().delay(800).fadeOut()
-				}, function(err) {
-					submitbutton.removeClass('disabled').addClass('enabled').text('Отправить').on('click', sendOrder);
-					$('#return-message').text('Произошла ошибка').css('color','red').fadeIn().delay(800).fadeOut()
-					console.log("FAILED. error=", err);
-				}
-			);
-		}else{
-			$('#return-message').css('color', 'red').text('Пожалуйста, заполните все поля').fadeIn().delay(700).fadeOut();
-		}
-	};
+
 	function sendMessage(e){
-		var email = {};
-		var isValid = 4;
-		var contact_return_message = $('.contact-form-returned-message');
-		$('.contact-form .form-input').each(function() {
-			if(($.trim($(this).val())).length == 0){
-				--isValid;
+		var self = $(this);
+		console.log(self);
+		if (e.target.matches('.submit-button') ){
+			var submitbutton = self.find('.submit-button');
+			var email = {};
+			var inputs = self.find('.form-input');
+			var isValid = inputs.length;
+			console.log(isValid);
+			var filled = isValid;
+			var message = self.children('.form-message');
+			inputs.each(function() {
+				if(($.trim($(this).val())).length == 0){
+					--filled;
+					return;
+				};
+				email[$(this).attr('name')] = $(this).val();
+			});
+			if (filled < isValid){
+				message.text('Пожалуйста, заполните все поля').css('color','red').fadeIn().delay(800).fadeOut();
 				return;
-			};
-			email[$(this).attr('name')] = $(this).val();
-		});
-		if (isValid < 4){
-			contact_return_message.text('Пожалуйста, заполните все поля').css('color','red').fadeIn().delay(800).fadeOut();
-			return;
-		}else{
-			console.log(email);
-			var submitbutton = $('#contact-form-submit');
-			submitbutton.off('click', sendMessage).removeClass('enabled').addClass('disabled').text('Полождите...');
-			emailjs.send('test_email','test_email_template', email)
-			.then(function(response) {
-					console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
-					submitbutton.removeClass('disabled').addClass('enabled').text('Отправить').on('click', sendMessage);
-					contact_return_message.text('Сообщение отправлено').css('color','#45e45b').fadeIn().delay(800).fadeOut()
-				}, function(err) {
-					submitbutton.removeClass('disabled').addClass('enabled').text('Отправить').on('click', sendMessage);
-					contact_return_message.text('Произошла ошибка').css('color','red').fadeIn().delay(800).fadeOut()
-					console.log("FAILED. error=", err);
-				}
-			);
+			}else{
+				console.log(email);
+				self.off('click', sendMessage)
+				submitbutton.removeClass('enabled').addClass('disabled').text('Полождите...');
+				emailjs.send('test_email','test_email_template', email)
+				.then(function(response) {
+						console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+						submitbutton.removeClass('disabled').addClass('enabled').text('Отправить')
+						message.text('Сообщение отправлено').css('color','#45e45b').fadeIn().delay(800).fadeOut()
+						self.on('click', sendMessage);
+					}, function(err) {
+						submitbutton.removeClass('disabled').addClass('enabled').text('Отправить')
+						self.on('click', sendMessage);
+						message.text('Произошла ошибка').css('color','red').fadeIn().delay(800).fadeOut()
+						console.log("FAILED. error=", err);
+					}
+				);
+			}
 		}
 	};
-	$('#order-form-submit').on('click', sendOrder);
-	$('#contact-form-submit').on('click', sendMessage);
+	$('.form').on('click', sendMessage);
 	$('.popup-window-close-button').click(function(e) {
 		$('.hover-block').css("display", "none").css('opacity', '0');
 			$('.popup-window').animate({top: "-=100px"})
